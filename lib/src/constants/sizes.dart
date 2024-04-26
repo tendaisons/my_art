fabed61ff92cc7a7b1c449612e72f1c653d269a1
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2024-04-26 05:48:43 +0200

    Features

--- lib/src/constants/sizes.dart
+++ lib/src/constants/sizes.dart
@@ -1,4 +1,6 @@
 // App default Sizing
+import 'package:flutter/material.dart';
+
 const tDefaultSize = 30.0;
 const tButtonHeight = 15.0;
 const tFormHeight = 30.0;
@@ -8,3 +10,9 @@ const tSplashContainerSize = 30.0;
 const tDashboardPadding = 20.0;
 const tDashboardCardPadding = 10.0;
 
+extension MediaQueryValues on BuildContext {
+  double get height => MediaQuery.of(this).size.height;
+  double get width => MediaQuery.of(this).size.width;
+  double get toPadding => MediaQuery.of(this).viewPadding.top;
+  double get bottom => MediaQuery.of(this).viewInsets.bottom;
+}
